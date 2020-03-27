ada77558d5ea9295119484010db0b62cc8bc112d
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-27 05:48:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,6 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    # season_text = season.to_s.capitalize + ":"
     puts season.to_s.capitalize + ":"
     holidays.each do |holiday, supplies|
       puts "  " + holiday.to_s.split("_").map(&:capitalize).join(" ") + ": " + supplies.join(", ")
