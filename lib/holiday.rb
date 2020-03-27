5e815c77761cd272bd60a825ee8e4bc3379090e2
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-27 06:01:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
   holidays_with_bbqs = []
   holiday_hash.each do |season, holidays|
     if holidays.has_value?("BBQ")
-      holidays_with_bbqs << holiday.key("BBQ")
+      holidays_with_bbqs << holidays.key("BBQ")
     end
   end
   holidays_with_bbqs
