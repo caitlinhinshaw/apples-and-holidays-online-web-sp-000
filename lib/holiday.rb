6f39891bdda09222d6d4b870a967696242935d80
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-27 06:07:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,10 +77,10 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holidays_with_bbqs = []
   holiday_hash.each do |season, holidays|
-    binding.pry
+    #binding.pry
     if holidays.values.include?("BBQ")
       # binding.pry
-      # holidays_with_bbqs << holidays.key("BBQ")
+      holidays_with_bbqs << holidays.key
     end
   end
   holidays_with_bbqs
